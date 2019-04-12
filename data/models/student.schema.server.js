712c75da1bdcabd7321650090eff78411ba16d8f
Yifang Zhao <yifangzhao@Yifangs-MBP.fios-router.home>
2019-04-11 23:45:26 -0400

    Created a Mongoose schema for Student class

---
+++ data/models/student.schema.server.js
@@ -0,0 +1,11 @@
+const mongoose = required('mongoose');
+const studentSchema = mongoose.Schema({
+    _id: Number,
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    gradYear: Number,
+    scholarship: {type: Number, min: 0}
+}, {collection: 'students'});
+module.exports = studentSchema;

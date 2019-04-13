45d889776a7a19892e16a0b904121f500ebe6983
Yifang Zhao <yifangzhao@Yifangs-MBP.fios-router.home>
2019-04-12 23:49:42 -0400

    Committed updates for deploying this application on heroku

--- data/db.js
+++ data/db.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    const databaseName = 'white-board';
+    // const databaseName = 'white-board';
     var connectionString = 'mongodb://heroku_q3s8bglg:9q8dcjag656vh4kn47b399akom@ds139576.mlab.com:39576/heroku_q3s8bglg/';
-    connectionString += databaseName;
+    // connectionString += databaseName;
     mongoose.connect(connectionString, { useNewUrlParser: true });
 };
